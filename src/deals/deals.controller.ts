b72907fe63a5babe3ed02bdbfb64de71761442e0
quaresmateo <teoian.giovane@gmail.com>
2021-12-30 17:59:27 -0300

    feat: :card_file_box: connected with database

--- src/deals/deals.controller.ts
+++ src/deals/deals.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { DealsService } from './deals.service';
-import { DealDocument } from './entities/deal.entity';
 
 @Controller('deals')
 export class DealsController {
   constructor(private readonly dealsService: DealsService) { }
 
   @Post()
-  async create(@Body() body: DealDocument) {
-    return this.dealsService.create(body);
+  async create(@Body() body: any) {
+    const createdDeal = await this.dealsService.create(body);
+    return true;
   }
 
   @Get()
--- src/deals/deals.module.ts
+++ src/deals/deals.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DealsService } from './deals.service';
 import { DealsController } from './deals.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Deal, DealSchema } from './entities/deal.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Deal.name, schema: DealSchema }])],
   controllers: [DealsController],
-  providers: [DealsService]
+  providers: [DealsService],
+  exports: [DealsService]
 })
 export class DealsModule { }
