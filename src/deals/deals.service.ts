0b34b15c736089a171436bd5671555e947da2b86
quaresmateo <teoian.giovane@gmail.com>
2022-01-02 14:41:44 -0300

    feat: :necktie: updates create method

--- src/deals/deals.service.ts
+++ src/deals/deals.service.ts
@@ -1,15 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Responses } from 'src/responses/responses.interface';
 import { Deal, DealDocument } from './entities/deal.entity';
 
 @Injectable()
 export class DealsService {
   constructor(@InjectModel(Deal.name) private dealModel: Model<DealDocument | any>,) { }
 
-  async create(body: DealDocument): Promise<any> {
-    const createdDeal = new this.dealModel(body);
-    return createdDeal.save();
+  async create(body: any): Promise<any> {
+    let response: Responses;
+    try {
+      const { current } = body;
+
+      const deal: Deal = {
+        id: current.id,
+        person_name: current.person_name,
+        title: current.title,
+        status: current.status,
+        value: current.value,
+        currency: current.currency,
+        won_time: current.won_time,
+      };
+
+      if (deal.status === 'won') {
+        const createdDeal = new this.dealModel(deal);
+        await createdDeal.save();
+
+        response = {
+          statusCode: HttpStatus.CREATED,
+          message: 'Deal has been created',
+          data: { createdDeal },
+        };
+
+        return response;
+      }
+
+      response = {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Deal has not been created',
+        data: { info: 'Deal status is not "won"' },
+      };
+
+      return response;
+    } catch (error) {
+      let response: Responses = {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Deal has not been created',
+        data: { error }
+      };
+
+      return response;
+    }
   }
 
   async findAll(): Promise<any> {
